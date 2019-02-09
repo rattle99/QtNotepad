33b7ceb71a4b484b23d470abd695080d2f01dfea
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-02-09 20:50:26 +0530

    Add basic methods

--- QtNotepad/mainwindow.cpp
+++ QtNotepad/mainwindow.cpp
@@ -78,3 +78,18 @@ void MainWindow::on_actionPaste_triggered()
 {
     ui->textEdit->paste();
 }
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
--- QtNotepad/mainwindow.h
+++ QtNotepad/mainwindow.h
@@ -37,6 +37,12 @@ private slots:
 
     void on_actionPaste_triggered();
 
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
     QString currentFile = "";
